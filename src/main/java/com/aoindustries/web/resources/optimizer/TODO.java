b60ef2e3626d64ecf5c46bde39b20e5568bad183
AO Industries, Inc <support@aoindustries.com>
2020-03-01 12:10:08 -0600

    Added skeleton Maven projects

---
+++ src/main/java/com/aoindustries/web/resources/optimizer/TODO.java
@@ -0,0 +1,32 @@
+/*
+ * ao-web-resources-optimizer - Automatic and dynamic web resource optimization via WRO4J.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-web-resources-optimizer.
+ *
+ * ao-web-resources-optimizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-web-resources-optimizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-web-resources-optimizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.web.resources.optimizer;
+
+/**
+ * TODO
+ *
+ * @author  AO Industries, Inc.
+ */
+public class TODO {
+
+}
