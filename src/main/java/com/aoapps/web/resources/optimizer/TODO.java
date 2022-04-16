2d63c4810f229d1ff811d1532aa0780f97cf7a96
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:19 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/web/resources/optimizer/TODO.java
+++ src/main/java/com/aoapps/web/resources/optimizer/TODO.java
@@ -1,6 +1,6 @@
 /*
  * ao-web-resources-optimizer - Automatic and dynamic web resource optimization via WRO4J.
- * Copyright (C) 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-web-resources-optimizer.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.web.resources.optimizer;
 
 /**
